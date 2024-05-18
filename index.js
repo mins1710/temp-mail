52c5a7dae0901c02765a534530df3057cbb7b0a6
mins1710 <tranhuy.dev17@gmail.com>
2024-05-18 20:39:56 +0700

    Refresh access token here

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const emailParser = require("./routes/email-parser");
+const cron = require('node-cron');
+const gmailTester = require("gmail-tester");
+const path = require("path");
 
 app.use(express.json());
 app.use('/api', emailParser);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+function refreshToken() {
+    try {
+        gmailTester.refresh_access_token(path.resolve(__dirname,"./credentials.json"),path.resolve(__dirname,"./token.json"));
+    } catch (error) {
+        console.error('Error refreshing token:', error);
+    }
+}
 
+cron.schedule('0 0 * * *', () => {
+    console.log('Refreshing token...');
+    refreshToken();
+});
+console.log('Cron job started. Token will be refreshed every 5 minutes.');
 module.exports = app;
--- routes/email-parser.js
+++ routes/email-parser.js
@@ -1,15 +1,23 @@
 const gmailTester = require("gmail-tester");
 const router = require("express").Router();
 const path = require("path");
+const cron = require('node-cron');
 
 router.get("/test", (req,res) => {
     return res.send("Hello world");
 })
 router.post("/read", async (req,res) => {
+   try {
     const options = req.body;
     const messages = await gmailTester.get_messages( path.resolve(__dirname, "../credentials.json"),path.resolve(__dirname, "../token.json"),options);
     return res.send(messages);
+   } catch (err){
+    return res.status(500).send(err.message);
+   }
 });
+
+// Function to refresh the token
+
 
 
 module.exports = router;
